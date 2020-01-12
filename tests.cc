dc0f3c24344d2b36ccc8a2684ca98dc1dfa56532
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2020-01-12 19:42:03 +0100

    init

---
+++ tests.cc
@@ -0,0 +1,24 @@
+#include "lib_playlist.h"
+
+#define BOOST_TEST_MODULE tests
+
+#include <iostream>
+#include <boost/test/included/unit_test.hpp>
+#include <boost/test/output_test_stream.hpp>
+
+struct cout_redirect {
+    cout_redirect(std::streambuf *new_buffer) : old(std::cout.rdbuf(new_buffer)) {}
+
+    ~cout_redirect() {
+        std::cout.rdbuf(old);
+    }
+
+private:
+    std::streambuf *old;
+};
+
+BOOST_AUTO_TEST_SUITE(lib_playlist)
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
